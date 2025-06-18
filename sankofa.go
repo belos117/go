428ebd5b2143595ef33ac3b785ade1d13c9c386e
stormhenkel <belos117@gmail.com>
2025-06-18 17:07:32 +0200

    Added Module

---
+++ sankofa.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Henkel"
+	fmt.Println("Hello, Go World " + name)
+	fmt.Println(name, "How are you")
+	fmt.Println("Welcome to the Go programming language!")
+	fmt.Println("This is a simple program to demonstrate basic syntax.")
+}
